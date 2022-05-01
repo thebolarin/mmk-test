a9343d000ed68582e5e38a7ce21e26726d06be34
Moses Odutusin <odutusinmoses@gmail.com>
2022-05-01 21:37:34 +0100

    fixed request type

--- src/controllers/account.ts
+++ src/controllers/account.ts
@@ -3,22 +3,20 @@ import express, { Request, Response } from 'express';
 import { ResMsg } from '../util';
 import { getAll, inBoundSMS, outBoundSMS } from '../services/index';
 
-type Indexed = {
-    [key: string]: any;
-};
+type CustomRequest = Request & { [key:string]: any }
 
 export const index: any = async (req: Request, res: Response) => {
     return await getAll(res, req.query);
 }
 
-export const processInboundSMS: any = async (req: Indexed, res: Response) => {
+export const processInboundSMS: any = async (req: CustomRequest, res: Response) => {
     const result = await validateSMSRequest(req.body);
     if(result.status === false) return ResMsg(res, 400, result.message, '');
 
     return await inBoundSMS(res, req.body, req.decoded);
 }
 
-export const processOutboundSMS: any = async (req: Indexed, res: Response) => {
+export const processOutboundSMS: any = async (req: CustomRequest, res: Response) => {
     const result = await validateSMSRequest(req.body);
     if(result.status === false) return ResMsg(res, 400, result.message, '');
 
